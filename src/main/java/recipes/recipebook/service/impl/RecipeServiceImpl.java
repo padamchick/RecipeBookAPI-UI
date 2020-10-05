2d9ff68ed69dfe643ab264547db5695c7f996a4e
Piotrek <piotrek.adamczykk@gmail.com>
2020-10-05 19:32:42 +0200

    Implement CRUD operations for Recipe class

--- src/main/java/recipes/recipebook/service/impl/RecipeServiceImpl.java
+++ src/main/java/recipes/recipebook/service/impl/RecipeServiceImpl.java
@@ -1,8 +1,39 @@
 package recipes.recipebook.service.impl;
 
 import org.springframework.stereotype.Service;
+import recipes.recipebook.entity.Recipe;
+import recipes.recipebook.repository.RecipeRepository;
 import recipes.recipebook.service.RecipeService;
+
+import java.util.List;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
+
+    private RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public Recipe saveRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+        return recipeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Recipe> findAll() {
+        return recipeRepository.findAll();
+    }
+
+    @Override
+    public Recipe delete(Long id) {
+        final Recipe toDelete = recipeRepository.findById(id).orElseThrow(() -> new RuntimeException("Recipe no. " + id + " not exist! "));
+        recipeRepository.delete(toDelete);
+        return toDelete;
+    }
 }
