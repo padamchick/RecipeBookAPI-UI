f843dd5f3a82c96265704af2af659959a316c210
Piotr Adamczyk <piotrek.adamczykk@gmail.com>
2021-03-08 00:07:46 +0100

    Registration component cleaning

--- UI/src/app/auth/register/register.component.ts
+++ UI/src/app/auth/register/register.component.ts
@@ -15,9 +15,9 @@ import {first, tap} from 'rxjs/operators';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.less']
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent {
 
-  error: string[] = [];
+  errors: string[] = [];
   hide = true;
   rememberMe: boolean = false;
   @ViewChild('f', { static: false }) form: NgForm
@@ -29,26 +29,15 @@ export class RegisterComponent implements OnInit{
               private actions$: Actions) {
   }
 
-  ngOnInit(): void {
-    // this.actions$.pipe(
-    //   ofType(authActions.signUpFail))
-    //   .subscribe(({error}) => {
-    //   this.handleError(error);
-    // })
-  }
 
   onSubmit(form: NgForm) {
     if (!form.valid) {
       return;
     }
-    this.error = [];
-    console.log('Form', form);
+    this.errors = [];
     const username = form.value.username;
     const password = form.value.password;
     const confirmPassword = form.value.confirmPassword;
-
-    // const firstName = form.value.firstName;
-    // const lastName = form.value.lastName;
     const email = form.value.email;
 
     this.spinner.show()
@@ -59,30 +48,22 @@ export class RegisterComponent implements OnInit{
       first())
       .subscribe(({error}) => {
         this.handleError(error);
-        // this.form.reset();
         for (let name in this.form.controls) {
-          if(!(name === "username" && this.error.includes("USERNAME_ALREADY_TAKEN")
-          || name === "password" && this.error.includes("PASSWORDS_NOT_EQUAL")
-          || name === "confirmPassword" && this.error.includes("PASSWORDS_NOT_EQUAL")
-          || name === "email" && this.error.includes("EMAIL_ALREADY_TAKEN"))) {
+          if(!(name === "username" && this.errors.includes("USERNAME_ALREADY_TAKEN")
+          || name === "password" && this.errors.includes("PASSWORDS_NOT_EQUAL")
+          || name === "confirmPassword" && this.errors.includes("PASSWORDS_NOT_EQUAL")
+          || name === "email" && this.errors.includes("EMAIL_ALREADY_TAKEN"))) {
             this.form.controls[name].setErrors(null);
           } else {
             this.form.controls[name].reset();
           }
-
-
-
         }
       })
-
-
   }
 
   handleError(error) {
-    console.log('error', error)
     if(error.error && error.error.errors) {
-      error.error.errors.forEach(error => this.error.push(error.defaultMessage));
+      error.error.errors.forEach(error => this.errors.push(error.defaultMessage));
     }
-    console.log('this.error', this.error)
   }
 }
